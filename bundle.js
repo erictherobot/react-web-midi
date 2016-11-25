704df8b3c28de17fdaadb0fdcb2f4081252e119f
erictherobot <erictherobot@gmail.com>
2016-11-25 12:50:52 -0500

    Update 2016-11-25T17:50:51.500Z

--- bundle.js
+++ bundle.js
@@ -1,3 +1,4 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -361,3 +362,91 @@ exports['default'] = function () {
 };
 
 module.exports = exports['default'];
+
+},{}],"react-web-midi":[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _libMidiScript = require('./lib/MidiScript');
+
+var _libMidiScript2 = _interopRequireDefault(_libMidiScript);
+
+var propTypes = {};
+
+var defaultProps = {};
+
+var ReactWebMidi = (function (_Component) {
+  _inherits(ReactWebMidi, _Component);
+
+  function ReactWebMidi() {
+    _classCallCheck(this, ReactWebMidi);
+
+    _get(Object.getPrototypeOf(ReactWebMidi.prototype), 'constructor', this).apply(this, arguments);
+  }
+
+  _createClass(ReactWebMidi, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      (0, _libMidiScript2['default'])();
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement(
+        'div',
+        null,
+        _react2['default'].createElement(
+          'div',
+          { id: 'content' },
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'q', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'w', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'e', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'r', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 't', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'y', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'u', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'i', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'o', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': 'p', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': '[', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' }),
+          _react2['default'].createElement('div', { className: 'button', 'data-key': ']', 'data-sound': 'https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3' })
+        ),
+        _react2['default'].createElement(
+          'div',
+          { id: 'device_info' },
+          _react2['default'].createElement('div', { id: 'key_data' }),
+          _react2['default'].createElement('div', { id: 'inputs' }),
+          _react2['default'].createElement('div', { id: 'outputs' }),
+          _react2['default'].createElement('div', { id: 'displayNotes' })
+        )
+      );
+    }
+  }]);
+
+  return ReactWebMidi;
+})(_react.Component);
+
+exports['default'] = ReactWebMidi;
+
+ReactWebMidi.propTypes = propTypes;
+ReactWebMidi.defaultProps = defaultProps;
+module.exports = exports['default'];
+
+},{"./lib/MidiScript":1,"react":undefined}]},{},[]);
